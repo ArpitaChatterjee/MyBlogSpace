d6228e5ea432a6f2668205e2ce566cd1dd68867d
chatterjee__ <48859001+ArpitaChatterjee@users.noreply.github.com>
2020-11-19 13:04:39 +0530

    Update contactus.php

--- php/contactus.php
+++ php/contactus.php
@@ -6,7 +6,7 @@ if (isset($_REQUEST['name'],$_REQUEST['email'])) {
       $message = $_REQUEST['message'];
         
       // Set your email address where you want to receive emails. 
-      $to = 'arpitachatterjee@gmail.com';
+      $to = 'arpitachatterjee2510@gmail.com';
         
       $subject = 'Contact Request From Website';
       $headers = "From: ".$name." <".$email."> \r\n";
